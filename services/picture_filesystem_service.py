18a4aee9683a06b19c12d2abca1da8b443bb45d6
AlicePeng <aliceyufeipeng@gmail.com>
2021-12-10 22:20:47 +0800

    feat(services) Add save and get image function
    Add save image function. Image would be save on local file system
    and its path link and user info would be save to database.
    Add load image function. Use user_id to get the user's all images'
    path/url in a list.

--- services/picture_filesystem_service.py
+++ services/picture_filesystem_service.py
@@ -1,4 +1,8 @@
 import os
+from typing import List
+
+from daos.user_firestore_dao import UserFirestoreDao
+from models.user import User
 
 from linebot import (
     LineBotApi
@@ -26,13 +30,27 @@ class PictureFilesystemService:
         message_content = cls.line_bot_api.get_message_content(message_id=image_id)
 
         user_id = message_event.source['userId']
-        image_path = f'./{user_id}/{image_id}.png'
+        image_path = f'./images/{user_id}/{image_id}.png'
 
+        # 將檔案存到本地的檔案系統中
         with open(image_path, 'wb') as fwb:
             fwb.write(message_content.content)
 
+        # 將其路徑存到該使用者的資料庫檔案中
+        user = UserFirestoreDao.get_user(user_id)
+        if user.image_files:
+            files = user.image_files
+            user.image_files = files.append(image_path)
+        else:
+            user.image_files = list[image_path]
+        UserFirestoreDao.update_user(user)
 
+        return "OK"
 
     @classmethod
-    def get_pic_from_filesystem(cls):
-        pass
+    def get_pic_from_filesystem(cls, user_id: str) -> List[str]:
+
+        user = UserFirestoreDao.get_user(user_id)
+        user_image_files = user.image_files
+
+        return user_image_files
