c51c62931756618ebb7758df8e8c74d40c03bebb
AlicePeng <aliceyufeipeng@gmail.com>
2021-12-10 14:24:13 +0800

    fix(models) Add more fuction in User
    Add from_dict, to_dict and __repr__ fuction to User

--- models/user.py
+++ models/user.py
@@ -1,8 +1,10 @@
+from __future__ import annotations
+
 """
 User 的實體
+提供 from_dict 和 to_dict 方便快速轉換
+提供 __repr__ 打印參數
 """
-
-
 class User(object):
 
     def __init__(self, line_user_id, line_user_pic_url,  line_user_nickname, line_user_status,
@@ -14,3 +16,36 @@ class User(object):
         self.line_user_status = line_user_status
         self.line_user_system_language = line_user_system_language
         self.blocked = blocked
+
+    @staticmethod
+    def from_dict(user: dict) -> User:
+        user = User(
+            line_user_id=user.get(u'line_user_id'),
+            line_user_pic_url=user.get(u'line_user_pic_url'),
+            line_user_nickname=user.get(u'line_user_nickname'),
+            line_user_status=user.get(u'line_user_status'),
+            line_user_system_language=user.get(u'line_user_system_language'),
+            blocked=user.get(u'blocked')
+        )
+        return user
+
+    def to_dict(self):
+        user_dict = {
+            "line_user_id": self.line_user_id,
+            "line_user_pic_url": self.line_user_pic_url,
+            "line_user_nickname": self.line_user_nickname,
+            "line_user_status": self.line_user_status,
+            "line_user_system_language": self.line_user_system_language,
+            "blocked": self.blocked
+        }
+        return user_dict
+
+    def __repr__(self):
+        return (f'''User(
+            line_user_id={self.line_user_id},
+            line_user_pic_url={self.line_user_pic_url},
+            line_user_nickname={self.line_user_nickname},
+            line_user_status={self.line_user_status},
+            line_user_system_language={self.line_user_system_language},
+            blocked={self.blocked}
+        )''')
