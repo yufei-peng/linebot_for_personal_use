cc3380bc64d3969e06746484ce9b9e386e75045b
AlicePeng <aliceyufeipeng@gmail.com>
2021-12-19 22:47:30 +0800

    feat(config): add more key in config

--- config.py
+++ config.py
@@ -1,1 +1,40 @@
-MODEL = 'FirebaseUser'
+''' 選擇要使用的 user model，必須搭配 dao 使用
+    FirebaseUser -> FirebaseDao 使用
+    MysqlUser -> MysqlDao 使用
+'''
+MODEL: str = 'FirebaseUser'
+
+
+''' 選擇要使用的 資料庫
+    UserFirestoreDao
+    UserMysqlDao
+'''
+USERDAO: str = 'UserFirestoreDao'
+
+
+''' 選擇 文字訊息 要使用的 存檔地點
+    MessageCloudstorageService
+    MessageFilesystemService
+'''
+MESSAGESERVICE: str = 'MessageCloudstorageService'
+
+
+''' 選擇 照片 要使用的 存檔地點
+    ImageCloudstorageService
+    ImageFilesystemService
+'''
+IMAGESERVICE: str = 'ImageCloudstorageService'
+
+
+''' 選擇 語音檔案 要使用的 存檔地點
+    AudioCloudstorageService
+    AudioFilesystemService
+'''
+AUDIOSERVICE: str = 'AudioCloudstorageService'
+
+
+''' 選擇 影片檔案 要使用的 存檔地點
+    VideoCloudstorageService
+    VideoFilesystemService
+'''
+VIDEOSERVICE: str = 'VideoCloudstorageService'
